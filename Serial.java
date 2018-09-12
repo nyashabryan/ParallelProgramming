1955a137d4aaf0e9cae7559053f7a3bcaa669ff3
Nyasha <nyashabryan23@gmail.com>
2018-09-12 04:58:17 +0100

    Added more functions to the program

--- Serial.java
+++ Serial.java
@@ -1,7 +1,39 @@
 public class Serial{
 
+    private int terrainXSize;
+    private int terrainYSize;
+    List<Float> sunlightHours;
+    int numberOfTrees;
+    List<Trees> trees;
+
     public static void main(String[] args){
 
         System.out.println();
+        LoadMap();
+
+    }
+
+    public static void LoadMap(){
+        Scanner scanner = new Scanner(System.in);
+
+    }
+
+
+    public static boolean checkMapLoaded(){
+        return (sunlightHours || terrainXSize || terrainYSize ||
+            numberOfTrees || trees )
+    }
+
+    public class Tree{
+        
+        int xCorner;
+        int yCorner;
+        int canopy;
+        float sunlight;
+        public Tree(int xCorner, int yCorner, int canopy){
+            this.xCorner = xCorner;
+            this.yCorner = yCorner;
+            this.canopy = canopy;
+        }
     }
 }
