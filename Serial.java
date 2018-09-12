7fdd55885fd4ccc50def941314229f42c8b88514
Nyasha <nyashabryan23@gmail.com>
2018-09-12 05:18:51 +0100

    Added more functions to the program

--- Serial.java
+++ Serial.java
@@ -5,17 +5,48 @@ public class Serial{
     List<Float> sunlightHours;
     int numberOfTrees;
     List<Trees> trees;
+    float[][] gridSunlightHours;
 
     public static void main(String[] args){
 
         System.out.println();
         LoadMap();
 
+        calculateTreeHours();
+        int average = calculateSunlightAverage();
+        printResults();
+
     }
 
     public static void LoadMap(){
         Scanner scanner = new Scanner(System.in);
 
+    }
+
+    public static void calculateTreeHours(){
+        for (Tree tree: this.trees){
+            for (int i = tree.xCorner; i < tree.xCorner + tree.canopy;
+                i++){
+                if (i > terrainXSize - 1) continue;
+                for (int j = tree.yCorner; i < tree.yCorner + tree.canopy;
+                i++){
+                    if (j > terrainYSize -1) continue;
+                    tree.sunlight += gridSunlightHours[i][j];
+                }
+                
+                }
+        }
+    }
+    /**
+    
+     */
+    public static float calculateSunlightAverage(){
+        float total = 0;
+        for (Tree tree: this.trees){
+            total += trees.sunlight;
+        }
+
+        return total /((float)numberOfTrees)
     }
 
 
